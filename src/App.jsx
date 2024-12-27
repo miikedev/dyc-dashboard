af17eb0a4e4cf4f90d829c5647fa3362a624500b
mikedevv <maungdevv@gmail.com>
2024-12-27 15:37:40 +0700

    fix: import in app

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom'
 import Layout from './components/layout'
 import Dashboard from './pages/dashboard'
 import Settings from './pages/setting'
-import Auth from './pages/Auth'
+import Auth from './pages/auth'
 export default function App() {
   return (
     <BrowserRouter>
