af01bddf8d17da34abe3a118dc47679f6e43699e
mikedevv <maungdevv@gmail.com>
2025-01-02 11:47:58 +0700

    feat: add module page

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom'
-import Layout from './components/layout'
+
 import AuthLayout from './components/authLayout'
-import Dashboard from './pages/dashboard'
-import Settings from './pages/setting'
+import Layout from './components/layout'
+import Course from './pages/course'
 import Login from './pages/login'
 import Register from './pages/register'
+import Settings from './pages/setting'
+import Module from './pages/module'
 
 // This is a mock function. Replace it with your actual auth check
 const isAuthenticated = () => {
@@ -35,13 +37,14 @@ export default function App() {
             </ProtectedRoute>
           }
         >
-          <Route index element={<Dashboard />} />
+          <Route path="course" element={<Course />} />
+          <Route path="module" element={<Module />} />
           <Route path="settings" element={<Settings />} />
-          <Route path="courses" element={<Dashboard />} />
-          <Route path="recent" element={<Dashboard />} />
-          <Route path="my-courses" element={<Dashboard />} />
-          <Route path="manage" element={<Dashboard />} />
-          <Route path="enrollment" element={<Dashboard />} />
+          <Route path="courses" element={<Course />} />
+          <Route path="recent" element={<Course />} />
+          <Route path="my-courses" element={<Course />} />
+          <Route path="manage" element={<Course />} />
+          <Route path="enrollment" element={<Course />} />
           {/* Redirect any unmatched routes to the dashboard */}
           <Route path="*" element={<Navigate to="/dashboard" replace />} />
         </Route>
---
+++ src/data/modules.js
@@ -0,0 +1,58 @@
+export const modules = [
+    {
+      id: "001",
+      title: "Module_001",
+      activity: "24 Activity",
+      editedBy: "user name",
+      dateModified: "4/10/2024 19:31"
+    },
+    {
+      id: "002",
+      title: "Module_002",
+      activity: "24 Activity",
+      editedBy: "user name",
+      dateModified: "4/10/2024 19:31"
+    },
+    {
+      id: "003",
+      title: "Module_003",
+      activity: "24 Activity",
+      editedBy: "user name",
+      dateModified: "4/10/2024 19:31"
+    },
+    {
+      id: "004",
+      title: "Module_004",
+      activity: "24 Activity",
+      editedBy: "user name",
+      dateModified: "4/10/2024 19:31"
+    },
+    {
+      id: "005",
+      title: "Module_005",
+      activity: "24 Activity",
+      editedBy: "user name",
+      dateModified: "4/10/2024 19:31"
+    },
+    {
+      id: "006",
+      title: "Module_006",
+      activity: "24 Activity",
+      editedBy: "user name",
+      dateModified: "4/10/2024 19:31"
+    },
+    {
+      id: "007",
+      title: "Module_007",
+      activity: "24 Activity",
+      editedBy: "user name",
+      dateModified: "4/10/2024 19:31"
+    },
+    {
+      id: "008",
+      title: "Module_008",
+      activity: "24 Activity",
+      editedBy: "user name",
+      dateModified: "4/10/2024 19:31"
+    }
+  ]
